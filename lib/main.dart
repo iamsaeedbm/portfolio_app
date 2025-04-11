a739f2e2e481a83f56bc1d1c2c41d2ac24bde908
saeedbm <iamsaeedbm@gmail.com>
2025-04-11 22:40:27 +0330

    add & modified files

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,166 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: ThemeData(fontFamily: 'vazir'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: _getAppBar(),
+        body: SafeArea(
+          child: _getMainBody(),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+// appbar method
+  PreferredSizeWidget _getAppBar() {
+    return AppBar(
+      backgroundColor: Colors.deepPurple.shade100,
+      title: Text(
+        'Portfolio',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+// main body method
+  Widget _getMainBody() {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(width: double.infinity),
+          _getHeader(),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+
+  Widget _getHeader() {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: CircleAvatar(
+            backgroundImage: AssetImage('images/profile.png'),
+            radius: 70.0,
+          ),
+        ),
+        Text(
+          'Flutter developer',
+          style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 15.0),
+        Text(
+          'Lorem ipsum dolor sit amet. Ea perferendis porro nam molestiae distinctio aut nobis molestias et ipsum ullam.',
+          style: TextStyle(fontSize: 14.0),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 12.0),
+        _getRowIcons(),
+        SizedBox(height: 12.0),
+        _getSkilsCard(),
+        SizedBox(height: 12.0),
+        _getResume(),
+      ],
+    );
+  }
+
+  Widget _getRowIcons() {
+    return Wrap(
+      alignment: WrapAlignment.center,
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: FaIcon(FontAwesomeIcons.instagram),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: FaIcon(FontAwesomeIcons.telegram),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: FaIcon(FontAwesomeIcons.linkedin),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: FaIcon(FontAwesomeIcons.github),
+        ),
+      ],
+    );
+  }
+
+  Widget _getSkilsCard() {
+    var list = ['Android', 'Dart', 'Flutter', 'Java', 'Kotlin'];
+
+    return Wrap(
+      children: [
+        for (var skill in list)
+          Card(
+            elevation: 6.0,
+            shadowColor: Colors.purple,
+            child: Column(
+              children: [
+                Container(
+                  height: 80.0,
+                  width: 80.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image(
+                      image: AssetImage('images/$skill.png'),
+                    ),
+                  ),
+                ),
+                Text('$skill'),
+              ],
             ),
+          )
+      ],
+    );
+  }
+
+  Widget _getResume() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+        width: double.infinity,
+        color: Colors.deepPurple.shade100,
+        child: Column(
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Experience',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(width: double.infinity),
+                Text(
+                    'Lorem ipsum dolor sit amet. Ea porro nam  distinctio aut nobis molestias et ipsum.'),
+                Text(
+                    'Lorem ipsum dolor sit amet. Ea porro nam  distinctio aut nobis molestias et ipsum.'),
+                Text(
+                    'Lorem ipsum dolor sit amet. Ea porro nam  distinctio aut nobis molestias et ipsum.'),
+                Text(
+                    'Lorem ipsum dolor sit amet. Ea porro nam  distinctio aut nobis molestias et ipsum.'),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
